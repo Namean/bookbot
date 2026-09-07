fba2c0f078c6147c3056859d497ff024146164cf
namean <dwilsoncnc@gmail.com>
2026-09-07 19:57:00 -0400

    feat(lesson): implement get_book_text function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+from pathlib import Path
+
+def get_book_text(path_to_file) -> str:
+    with open(path_to_file, mode='r', encoding='utf-8') as fp:
+        file_contents = fp.read()
+        return file_contents
+
+def main():
+    book_path = Path("books/frankenstein.txt")
+    print( get_book_text(book_path) )
+
+if __name__ == '__main__':
+    main()
