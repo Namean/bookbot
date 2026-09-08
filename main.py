094246e6e5a23d20f18edfdd46d4855da47be511
namean <dwilsoncnc@gmail.com>
2026-09-07 21:05:00 -0400

    feat(lesson): add character count function

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+import sys
 from pathlib import Path
 
+
 def get_book_text(path_to_file) -> str:
-    with open(path_to_file, mode='r', encoding='utf-8') as fp:
-        file_contents = fp.read()
-        return file_contents
+    try:
+        with open(path_to_file, mode="r", encoding="utf-8") as fp:
+            file_contents = fp.read()
+            return file_contents
+    except FileNotFoundError:
+        print(f"file not found: {path_to_file}")
+        user_response = input(
+            f"Would you like to create file {path_to_file} [Y/n]?: "
+        ).lower()
+        if user_response == "y":
+            path_to_file.touch()
+            get_book_text(path_to_file)
+        else:
+            sys.exit(f"Error: file not found '{path_to_file}'")
+
+
+def count_words(book_text: str = None) -> int:
+    num_words = 0
+
+    if book_text is None:
+        return num_words 
+
+    num_words = len(book_text.split())
+    print(f"Found {num_words} total words")
+
+    return len(book_text.split())
+
 
 def main():
     book_path = Path("books/frankenstein.txt")
-    print( get_book_text(book_path) )
+    book_text = get_book_text(book_path)
+    count_words(book_text)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
